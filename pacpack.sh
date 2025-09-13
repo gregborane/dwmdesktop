d88e1a770ad594b192ac22c8942c1795a6594f40
gregborane <madaradellac@gmail.com>
2025-09-13 17:00:25 +0200

    new modifications

--- pacpack.sh
+++ pacpack.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 
 # Dependencies for my System
-sudo pacman -S --noconfirm --needed alacritty aspell alsa-plugins 
-sudo pacman -S --noconfirm --needed base-devel bash-completion 
-sudo pacman -S --noconfirm --needed cmake curl 
-sudo pacman -S --noconfirm --needed dkms dotnet-runtime-8.0 dotnet-runtime-7.0 
-sudo pacman -S --noconfirm --needed feh fd ffmpeg flameshot fuse-common fzf 
-sudo pacman -S --noconfirm --needed gamescope gnome-text-editor ghostscript gnutls giflip gimp git gtk3 gtk4
-sudo pacman -S --noconfirm --needed hdf5 htop hspell
+
+sudo pacman -S --noconfirm --needed alacritty alsa-plugins aspell
+sudo pacman -S --noconfirm --needed base-devel bash-completion
+sudo pacman -S --noconfirm --needed cmake curl
+sudo pacman -S --noconfirm --needed dkms dotnet-runtime-7.0 dotnet-runtime-8.0
+sudo pacman -S --noconfirm --needed feh ffmpeg flameshot fuse-common fzf
+sudo pacman -S --noconfirm --needed gamescope gimp giflip gnome-text-editor ghostscript git gtk3 gtk4
+sudo pacman -S --noconfirm --needed hdf5 hspell htop
 sudo pacman -S --noconfirm --needed i3lock imagemagick
-sudo pacman -S --noconfirm --needed lib32-giflib libpng lib32-libpng libldap lib32-libldap lib32-gnutls mpg123 lib32-mpg123 \
-lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error lib32-alsa-plugins alsa-lib lib32-alsa-lib \
-libjpeg-turbo lib32-libjpeg-turbo lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama \
-lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs \
-lib32-vulkan-icd-loader libvoikko
 sudo pacman -S --noconfirm --needed less libreoffice-fresh linux-lts linux-lts-docs linux-lts-headers lutris
 sudo pacman -S --noconfirm --needed mariadb mermaid-cli
 sudo pacman -S --noconfirm --needed ncurse neovim ninja noto-fonts nuspell nwg-look
 sudo pacman -S --noconfirm --needed openal openssh
 sudo pacman -S --noconfirm --needed pavucontrol picom python-neovim
-sudo pacman -S --noconfirm --needed qt5 qt6 
+sudo pacman -S --noconfirm --needed qt5 qt6
 sudo pacman -S --noconfirm --needed r ripgrep rofi
 sudo pacman -S --noconfirm --needed sddm sqlite sshfs steam
-sudo pacman -S --noconfirm --needed tectonic texlive thunar tree-sitter-cli thunderbird timeshift
-sudo pacman -S --noconfirm --needed unzip
-sudo pacman -S --noconfirm --needed vlc vulkan-icd-loader
-sudo pacman -S --noconfirm --needed wine-staging wget
-sudo pacman -S --noconfirm --needed zip zoxide
+sudo pacman -S --noconfirm --needed tectonic texlive thunar timeshift tree-sitter-cli thunderbird
+sudo pacman -S --noconfirm --needed unzip vlc vulkan-icd-loader
+sudo pacman -S --noconfirm --needed wget wine-staging zathura zip zoxide
+
+# lib* packages (alphabetically sorted)
+sudo pacman -S --noconfirm --needed lib32-alsa-lib lib32-alsa-plugins lib32-giflib lib32-gmp lib32-gnutls \
+lib32-libgpg-error lib32-libjpeg-turbo lib32-libldap lib32-libpulse lib32-libxcomposite lib32-libxinerama \
+lib32-libxslt lib32-mpg123 lib32-ncurses lib32-opencl-icd-loader lib32-sqlite libgcrypt libgpg-error \
+libjpeg-turbo libldap libpng libxcomposite libxinerama libxslt libva v4l-utils
+
